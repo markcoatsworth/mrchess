747a95cd3cce6f865022078de686e6c0f6b620a9
Mark Coatsworth <coatsworth@cs.wisc.edu>
2021-01-15 21:03:12 -0600

    Fixed tabs vs spaces

--- src/mrchess.cpp
+++ src/mrchess.cpp
@@ -10,31 +10,31 @@ using namespace std;
 
 int main() {
 
-	json requestJson;
-	json responseJson;
-	std::string requestMethod;
-	std::string postData;
+    json requestJson;
+    json responseJson;
+    std::string requestMethod;
+    std::string postData;
 
-	// If this is a POST request, read + parse input stream data
-	if (getenv("requestMethod")) {
-		requestMethod = getenv("requestMethod");
-		if (requestMethod == "POST") {
-			std::ifstream is(stdin);
-			char c;
-			while (is.get(c)) {
-				postData += c;
-			}
-			is.close();
-			requestJson = json::parse(postData);
-		}
-	}
+    // If this is a POST request, read + parse input stream data
+    if (getenv("requestMethod")) {
+        requestMethod = getenv("requestMethod");
+        if (requestMethod == "POST") {
+            std::ifstream is(stdin);
+            char c;
+            while (is.get(c)) {
+                postData += c;
+            }
+            is.close();
+            requestJson = json::parse(postData);
+        }
+    }
 
-	// Prepare response JSON
-	responseJson["availableMoves"] = { "pos1", "pos2", "pos3" };
+    // Prepare response JSON
+    responseJson["availableMoves"] = { "pos1", "pos2", "pos3" };
 
-	// Send response
-	cout << "Content-type: application/json" << endl << endl;
-	cout << responseJson.dump() << endl;
+    // Send response
+    cout << "Content-type: application/json" << endl << endl;
+    cout << responseJson.dump() << endl;
 
-	return 0;
+    return 0;
 }
