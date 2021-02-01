3874727fef9756c354afa84e8e9b7a62203ecdd4
Mark Coatsworth <coatsworth@cs.wisc.edu>
2021-01-31 20:57:32 -0600

    First pass at bitwise piece representation

--- src/Piece.cpp
+++ src/Piece.cpp
@@ -1,49 +1,116 @@
+#include <iostream>
 #include <string>
 
 #include "Piece.h"
 
 using namespace std;
 
-Piece::Piece(): 
-    _color(PieceColor::NONE), 
-    _type(PieceType::NONE) 
+Piece::Piece()
 {}
 
-Piece::Piece(PieceColor color, PieceType type):
-    _color(color), 
-    _type(type) 
-{}
+Piece::Piece(PieceColor color, PieceType type) {
+    // Set the piece color. We don't need to set the color for BLACK, since
+    // this is the default for any non-null piece.
+    if (color == PieceColor::WHITE) {
+        _piece |= _WHITE;
+    }
+
+    // Set the piece TYPE. Defaults to _NONE;
+    switch (type) {
+        case PieceType::PAWN:
+            _piece |= _PAWN;
+            break;
+        case PieceType::KNIGHT:
+            _piece |= _KNIGHT;
+            break;
+        case PieceType::BISHOP:
+            _piece |= _BISHOP;
+            break;
+        case PieceType::ROOK:
+            _piece |= _ROOK;
+            break;
+        case PieceType::QUEEN:
+            _piece |= _QUEEN;
+            break;
+        case PieceType::KING:
+            _piece |= _KING;
+            break;
+        default:
+            break;
+    }
+}
 
 Piece::Piece(const Piece& piece) {
-    _color = piece._color;
-    _type = piece._type;
+    _piece = piece._piece;
 }
 
 std::string Piece::toString() {
     std::string str = ".";
-    switch (_color) {
-        case PieceColor::WHITE: str = "W"; break;
-        case PieceColor::BLACK: str = "B"; break;
+ 
+    switch (getColor()) {
+        case PieceColor::WHITE:
+            str = "W";
+            break;
+        case PieceColor::BLACK:
+            str = "B";
+            break;
     }
-    switch (_type) {
-        case PieceType::PAWN:   str += "P"; break;
-        case PieceType::KNIGHT: str += "Kn"; break;
-        case PieceType::BISHOP: str += "B"; break;
-        case PieceType::ROOK:   str += "R"; break;
-        case PieceType::QUEEN:  str += "Q"; break;
-        case PieceType::KING:   str += "Ki"; break;
+    switch (getType()) {
+        case PieceType::PAWN:
+            str += "P";
+            break;
+        case PieceType::KNIGHT:
+            str += "Kn";
+            break;
+        case PieceType::BISHOP:
+            str += "B";
+            break;
+        case PieceType::ROOK:
+            str += "R";
+            break;
+        case PieceType::QUEEN:
+            str += "Q";
+            break;
+        case PieceType::KING:
+            str += "Ki";
+            break;
     }
     return str;
 }
 
-const std::string Piece::getPosition() {
-    return "";
-}
-
 const PieceColor Piece::getColor() {
-    return _color;
+    if ((_piece & _WHITE) == _WHITE) {
+        return PieceColor::WHITE;
+    }
+    // A black piece has the color bit = 0, but some other bits are set
+    else if ((_piece & _NONE) != _piece) {
+        return PieceColor::BLACK;
+    }
+    else {
+        return PieceColor::NONE;
+    }
 }
 
 const PieceType Piece::getType() {
-    return _type;
+    if ((_piece & _PAWN) == _PAWN) {
+        return PieceType::PAWN;
+    }
+    else if ((_piece & _KNIGHT) == _KNIGHT) {
+        return PieceType::KNIGHT;
+    }
+    else if ((_piece & _BISHOP) == _BISHOP) {
+        return PieceType::BISHOP;
+    }
+    else if ((_piece & _ROOK) == _ROOK) {
+        return PieceType::ROOK;
+    }
+    else if ((_piece & _QUEEN) == _QUEEN) {
+        return PieceType::QUEEN;
+    }
+    else if ((_piece & _KING) == _KING) {
+        return PieceType::KING;
+    }
+    else {
+        return PieceType::NONE;
+    }
 }
--- src/Piece.h
+++ src/Piece.h
@@ -1,11 +1,28 @@
 #pragma once
 
+#include <cstddef>
 #include <string>
 
 using namespace std;
 
 enum class PieceColor { NONE, BLACK, WHITE };
 enum class PieceType { NONE, PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING };
+
+/*
+ * Each piece is represented by a single byte, or eight bits.
+ * The bits correspond to the following (left to right):
+ * 1) has moved (0 = unmoved, 1 = moved)
+ * 2) color (0 = black, 1 = white)
+ * 3) 1 = is type Pawn
+ * 4) 1 = is type Knight
+ * 5) 1 = is type Bishop
+ * 6) 1 = is type Rook
+ * 7) 1 = is type Queen
+ * 8) 1 = is type King
+ *
+ * A piece of value 0 (0x00000000) represents an empty piece, does not exist
+ * 
+ */
 
 class Piece
 {
@@ -22,7 +39,7 @@ public:
     Piece(PieceColor color, PieceType type);
 
     /**
-     * Copy structor
+     * Copy constructor
      */
     Piece(const Piece& piece);
 
@@ -31,11 +48,6 @@ public:
      * (C = color, T = tyoe), or "." if none
      */
     std::string toString();
-
-    /**
-     * Return a string representation of the piece position (ie. "a4", "f5")
-     */
-    const std::string getPosition();
 
     /**
      * Return the color of piece
@@ -51,9 +63,17 @@ public:
 
   private:
 
-    // TODO: Color and type should be replaced with a single-byte char bitwise representation
-    PieceColor _color;
-    PieceType _type;
+      std::byte _piece{ 0b00000000 };
+
+      inline static std::byte _NONE     { 0b00000000 };
+      inline static std::byte _MOVED    { 0b00000001 };
+      inline static std::byte _WHITE    { 0b00000010 };
+      inline static std::byte _PAWN     { 0b00000100 };
+      inline static std::byte _KNIGHT   { 0b00001000 };
+      inline static std::byte _BISHOP   { 0b00010000 };
+      inline static std::byte _ROOK     { 0b00100000 };
+      inline static std::byte _QUEEN    { 0b01000000 };
+      inline static std::byte _KING     { 0b10000000 };
 
 };
 
