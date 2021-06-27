dd6e91c7dfe5dae762a3813a96cce307d96c57a2
Mark Coatsworth <coatsworth@cs.wisc.edu>
2021-06-26 22:23:15 -0500

    Added many tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -8,6 +8,8 @@
 #include "catch.hpp"
 #include "../src/Board.h"
 #include "../src/nlohmann/json.hpp"
+#include "../src/MinimaxNode.h"
+#include "../src/MoveEngine.h"
 
 using json = nlohmann::json;
 using namespace std;
@@ -18,24 +20,104 @@ TEST_CASE("Pawn legal moves") {
     json requestJson;
     json responseJson;
 
-    // Pawn starting from the opening line
-    requestJson["board"] = { {"b7", "black pawn"} };
-    board.setPieces(requestJson["board"]);
-    responseJson["moves"] = board.getPieceAvailableMoves("b7");
-    REQUIRE(responseJson["moves"] == json({"b7b6", "b7b5"}));
-    board.clear();
+    SECTION("Pawn starting from the opening line") {
+        requestJson["board"] = { {"b7", "black pawn"} };
+        board.setPieces(requestJson["board"]);
+        responseJson["moves"] = board.getPieceAvailableMoves("b7");
+        REQUIRE(responseJson["moves"] == json({ "b7b6", "b7b5" }));
+    }
+    SECTION("Pawn after a first move") {
+        requestJson["board"] = { {"b6", "black pawn"} };
+        board.setPieces(requestJson["board"]);
+        responseJson["moves"] = board.getPieceAvailableMoves("b6");
+        REQUIRE(responseJson["moves"] == json({ "b6b5" }));
+    }
+    SECTION("Pawn taking an opponent piece") {
+        requestJson["board"] = { {"b6", "black pawn"}, {"c5", "white pawn"} };
+        board.setPieces(requestJson["board"]);
+        responseJson["moves"] = board.getPieceAvailableMoves("b6");
+        REQUIRE(responseJson["moves"] == json({ "b6b5", "b6c5" }));
+    }
+}
 
-    // Pawn after a first move
-    requestJson["board"] = { {"b6", "black pawn"} };
-    board.setPieces(requestJson["board"]);
-    responseJson["moves"] = board.getPieceAvailableMoves("b6");
-    REQUIRE(responseJson["moves"] == json({"b6b5"}));
-    board.clear();
+TEST_CASE("Knight legal moves") {
+    Board board;
+    json requestJson;
+    json responseJson;
 
-    // Pawn taking an opponent piece
-    requestJson["board"] = { {"b6", "black pawn"}, {"c5", "white pawn"} };
-    board.setPieces(requestJson["board"]);
-    responseJson["moves"] = board.getPieceAvailableMoves("b6");
-    REQUIRE(responseJson["moves"] == json({ "b6b5", "b6c5" }));
-    board.clear();
+    SECTION("Knight starting from the opening line") {
+        requestJson["board"] = { {"b8", "black knight"}, {"d7", "black pawn"} };
+        board.setPieces(requestJson["board"]);
+        responseJson["moves"] = board.getPieceAvailableMoves("b8");
+        REQUIRE(responseJson["moves"] == json({ "b8a6", "b8c6" }));
+    }
+}
+
+TEST_CASE("Simple 1-move checkmates") {
+    Board board;
+    json requestJson;
+    json responseJson;
+    MoveEngine moveEngine;
+
+    SECTION("Two rooks checkmate") {
+        requestJson["board"] = { {"e1", "white king"}, {"b2", "black rook"}, {"a8", "black rook"} };
+        board.setPieces(requestJson["board"]);
+        responseJson["move"] = moveEngine.getMinimaxMove(board, Color::BLACK);
+        REQUIRE(responseJson["move"] == "a8a1!!");
+    }
+
+}
+
+TEST_CASE("Famous games 1-move checkmates") {
+    Board board;
+    json requestJson;
+    json responseJson;
+    MoveEngine moveEngine;
+
+    SECTION("Bobby Fischer Game of the Century 1956") {
+        requestJson["board"] = { 
+            {"b8", "white queen"},
+            {"f7", "black pawn"}, {"g7", "black king"},
+            {"c6", "black pawn"}, {"g6", "black pawn"},
+            {"b5", "black pawn"}, {"d5", "white knight"}, {"h5", "black pawn"},
+            {"b4", "black bishop"}, {"h4", "white pawn"},
+            {"b3", "black bishop"}, {"c3", "black knight"},
+            {"a2", "black rook"}, {"g2", "white pawn"},
+            {"c1", "white king"}
+        };
+        board.setPieces(requestJson["board"]);
+        responseJson["move"] = moveEngine.getMinimaxMove(board, Color::BLACK);
+        // Bobby Fischer played a2c2, but b4a3 is also a valid checkmate
+        bool checkmate = responseJson["move"] == "a2c2!!" || responseJson["move"] == "b4a3!!";
+        REQUIRE(checkmate);
+    }
+    SECTION("Janez Barle vs Iztok Jelen 1979") {
+        requestJson["board"] = {
+            {"a8", "black rook"}, {"f8", "black rook"}, {"h8", "black king"},
+            {"a7", "black pawn"}, {"b7", "black pawn"}, {"c7", "black pawn"}, {"d7", "black bishop"}, {"g7", "black pawn"}, {"h7", "black pawn"},
+            {"c4", "white knight"},
+            {"a3", "white pawn"}, {"c3", "white pawn"}, {"g3", "white queen"},
+            {"a2", "white bishop"}, {"b2", "white pawn"}, {"e2", "white rook"}, {"h2", "white pawn"},
+            {"a1", "white rook"}, {"e1", "black knight"}, {"h1", "white king"}
+        };
+        board.setPieces(requestJson["board"]);
+        responseJson["move"] = moveEngine.getMinimaxMove(board, Color::BLACK);
+        REQUIRE(responseJson["move"] == "f8f1!!");
+    }
+    SECTION("The King is Trapped, Ghinda vs Stohl 1985") {
+        requestJson["board"] = {
+            {"a8", "white queen"},
+            {"f7", "black pawn"}, {"g7", "black pawn"}, {"h7", "black king"},
+            {"e6", "black pawn"}, {"h6", "black pawn"},
+            {"d5", "black knight"},
+            {"e4", "white king"},
+            {"d3", "white rook"}, {"f3", "white pawn"},
+            {"b2", "black queen"}, {"e2", "white knight"}, {"g2", "white pawn"}, {"h2", "white pawn"}
+        };
+        board.setPieces(requestJson["board"]);
+        responseJson["move"] = moveEngine.getMinimaxMove(board, Color::BLACK);
+        REQUIRE(responseJson["move"] == "f7f5!!");
+    }
+
+
 }
